51a52f679218cdd3f780699b0f8aaf937eee3ae8
lhg <lhg520705@163.com>
2018-12-22 13:01:30 +0800

    face detect and profile line

---
+++ face/face_recog.py
@@ -0,0 +1,25 @@
+# coding= utf-8
+
+import face_recognition
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+known_image_kaka = face_recognition.load_image_file('kaka.jpg')
+known_image_ronaldo = face_recognition.load_image_file('ronaldo.jpg')
+
+kaka_face_encoding = face_recognition.face_encodings(known_image_kaka)[0]
+ronaldo_face_encoding = face_recognition.face_encodings(known_image_ronaldo)[0]
+
+know_encodings = [
+    kaka_face_encoding,
+    ronaldo_face_encoding
+]
+
+image_to_test = face_recognition.load_image_file('kaka2.jpg')
+image_to_test_encoding = face_recognition.face_encodings(image_to_test)[0]
+
+face_distances = face_recognition.face_distance(know_encodings,image_to_test_encoding)
+
+for i,face_distance in enumerate(face_distances):
+    print('the test image has a distance of {:.2} from known image #{}'.format(face_distance,i))
+    print('with a normal cutoff of 0.6,can the image match the known image? {}'.format(face_distance<0.6))
+    # print('with a normal cutoff of 0.5,can the image match the known image? {}'.format(face_distance<0.5))
